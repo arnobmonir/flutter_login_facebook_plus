9cb9017e15674c7722f8d7d29799e3de5489c5af
Arnob Monir <aarnob.monir@gmail.com>
2025-05-06 15:06:47 +0600

    changes

--- ios/Classes/FlutterLoginFacebookPlugin.m
+++ ios/Classes/FlutterLoginFacebookPlugin.m
@@ -1,11 +1,11 @@
 #import "FlutterLoginFacebookPlugin.h"
 #if __has_include(<flutter_login_facebook_plus/flutter_login_facebook-Swift.h>)
-#import <flutter_login_facebook/flutter_login_facebook-Swift.h>
+#import <flutter_login_facebook/flutter_login_facebook_plus-Swift.h>
 #else
 // Support project import fallback if the generated compatibility header
 // is not copied when this plugin is created as a library.
 // https://forums.swift.org/t/swift-static-libraries-dont-copy-generated-objective-c-header/19816
-#import "flutter_login_facebook-Swift.h"
+#import "flutter_login_facebook_plus-Swift.h"
 #endif
 
 @implementation FlutterLoginFacebookPlugin
